559b1e83abd652409f40d5bea5eecf064e838652
daniel <itzdanielmichael@gmail.com>
2023-06-10 00:50:00 -0400

    add gravity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -26,6 +26,14 @@ public class PlayerController : MonoBehaviour
     HandleMovement();
   }
 
+  // doesn't run every frame, runs every physics frame
+  void FixedUpdate()
+  {
+    HandleGravity();
+  }
+
+
+
   void HandleMovement()
   {
     // transform.position = new Vector3(
@@ -44,11 +52,28 @@ public class PlayerController : MonoBehaviour
     //   ) * Time.deltaTime
     // );
 
+    float yStore = moveAmount.y; // store the y value so we don't lose it when we reset moveAmount
+
     // move in the direction the camera is facing
     moveAmount = (cameraController.transform.forward * Input.GetAxisRaw("Vertical")) + (cameraController.transform.right * Input.GetAxisRaw("Horizontal"));
     moveAmount.y = 0f; // don't move up or down 
     moveAmount = moveAmount.normalized; // normalize so diagonal movement isn't faster
 
-    characterController.Move(moveAmount * moveSpeed * Time.deltaTime);
+    moveAmount.y = yStore; // reset the y value
+
+    characterController.Move(new Vector3(moveAmount.x * moveSpeed, moveAmount.y, moveAmount.z * moveSpeed) * Time.deltaTime);
+  }
+
+  private void HandleGravity()
+  {
+    if (!characterController.isGrounded)
+    {
+      // fixedDeltaTime how much time has passed since the last time fixedUpdate was called
+      moveAmount.y = moveAmount.y + (Physics.gravity.y * Time.fixedDeltaTime);
+    }
+    else
+    {
+      moveAmount.y = Physics.gravity.y * Time.deltaTime;
+    }
   }
 }
